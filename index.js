b9e23da626536eac42d330b0f6c9ebe28007c286
Joseph <jojoebrown129@gmail.com>
2020-06-23 08:32:53 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //This is the entry point of the web app
 const express = require('express');
-const fs = require('fs'); 
 app = express();
+
+let dataa = [
+          {fullname: "Buba Mohammed", SlackUsername: "@Mohammed", Track: "Backend", Email: "juniorbuba4real@gmail.com", points: 2},
+          {fullname: "Joseph Godwin", SlackUsername: "@Joebrown", Track: "Backend", Email: "shoo_shoo@gmail.com", points: 3},
+          {fullname: "Alabi Markaz", SlackUsername: "@Morkaz", Track: "Design", Email: "morkaz_alabi@yahoo.com", points: 1},
+          {fullname: "Cookie Brown", SlackUsername: "@cookie", Track: "Backend", Email: "cookie@gmail.com", points: 5},
+          {fullname: "Eric Jon", SlackUsername: "@eric", Track: "Backend",Email: "ericson@gmail.com", points: 6},
+          {fullname: "Jon Snow", SlackUsername: "@jonSnow", Track: "Backend",Email: "juniorbuba4real@gmail.com", points: 2}];
 
   function sortDescending(prop){
     return function (a, b){
@@ -27,12 +34,7 @@ function sortAscending(prop){
   return 0;
 }
 }
-let rawdataa = fs.readFileSync('./list.json');
-  let dataa = JSON.parse(rawdataa);
-  dataa.sort(sortDescending("points"));
 
-  app.get('./list.json', (req, res) => {
-    res.json(dataa);
-  });
+  dataa.sort(sortDescending("points"));
   console.log(dataa);
 
